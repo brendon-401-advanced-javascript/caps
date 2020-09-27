ceefd2a8786d37cb670a26ce9644c5aacf0ee891
BrendonLH <Brendonhampton1988@gmail.com>
2020-09-27 15:30:49 -0700

    added a gps folder to implement functionality

---
+++ gps/hub.js
@@ -0,0 +1,19 @@
+'use strict';
+const faker = require('faker');
+const events = require('../events');
+
+let direction = ['left', 'right', 'straight'];
+let newDirection =  direction[Math.floor(Math.random()*direction.length)];
+let address = faker.address.streetAddress();
+
+function gps(payload) {
+    let badGPS = direction[Math.floor(Math.random()*direction.length)];
+    console.log(badGPS);
+    console.log(payload.Location);
+    
+}
+
+
+events.on('direction', gps);
+
+events.emit('direction', { Address: address, Location: newDirection});
