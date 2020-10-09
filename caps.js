53550149e94a133513616f1854ff19078f73f3f8
BrendonLH <Brendonhampton1988@gmail.com>
2020-10-08 18:27:05 -0700

    added all vendor stuff to the vendor file

--- caps.js
+++ caps.js
@@ -1,18 +1,7 @@
 'use strict';
 
-const faker = require('faker');
-const events = require('./events.js');
 require('./vendor');
 require('./driver');
    
 
-setInterval(() => {
-    let stores = ['World of Wonders', 'Ivars Chowder House', 'Red Robin', 'Code Fellows'];
-    let storeName = stores[Math.floor(Math.random()*stores.length)]
-    let store_Num = faker.phone.phoneNumber();
-    let orderNum = faker.random.uuid();
-    let customerName = faker.name.findName();
-    let customerAddress = faker.address.streetAddress();
-    events.emit('create', { event:'Pickup', time: Date(Date.now), payload: { store: store_Num, store_Name: storeName, order_ID: orderNum, customer: customerName, address: customerAddress }});
-}, 7000);  
 
